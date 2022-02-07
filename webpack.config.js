7d14736ae07a5bc9b7cb33b7c2db2c67e990d38c
Josh Yee <joshyee@gmail.com>
2022-02-06 16:53:22 -0800

    bundle site

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
+const path = require('path');
+
 module.exports = {
-    module: {
-      rules: [
-        {
-          test: /\.css$/i,
-          use: ["style-loader", "css-loader"],
-        },
-      ],
-    },
-  };
+  entry: './src/index.js',
+  entry: {
+    index: './src/index.js',
+  },
+   output: {
+    filename: 'bundle.js',
+     path: path.resolve(__dirname, 'dist'),
+   },
+ };
